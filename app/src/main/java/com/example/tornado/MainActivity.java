5ab31336b9d76e6ff1311fad27d7cc09dc2a0933
Sanabel8 <sanabeljamal97@gmail.com>
2021-09-04 14:20:14 +0300

    start app

---
+++ app/src/main/java/com/example/tornado/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tornado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addDishButton = (Button) findViewById(R.id.addDishBtn);
+        addDishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddDish = new Intent(MainActivity.this,AddDish.class);
+                startActivity(goToAddDish);
+            }
+        });
+
+        Button menuButton = (Button) findViewById(R.id.menuBtn);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToMenu = new Intent(MainActivity.this,Menu.class);
+                startActivity(goToMenu);
+            }
+        });
+    }
+}
